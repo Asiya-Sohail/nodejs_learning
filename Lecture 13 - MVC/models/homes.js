81b81b5c1b8d26890123c9372173d68ed56f45c9
Asiya Sohail <sohailasiya27@gmail.com>
2025-08-26 12:16:58 +0500

    Lecture 13 done, Practice Set left

---
+++ Lecture 13 - MVC/models/homes.js
@@ -0,0 +1,49 @@
+// Core Modules
+const fs = require('fs')
+const path = require('path')
+const rootDir = require('../utils/pathUtils')
+
+// // Fake Database
+// const RegisteredHomes = []
+
+module.exports = class Home {
+  constructor(housName, price, location, rating, photoUrl) {
+    this.housName = housName
+    this.price = price
+    this.location = location
+    this.rating = rating
+    this.photoUrl = photoUrl
+  }
+
+  save() {
+    Home.fetchAll(RegisteredHomes => {
+      RegisteredHomes.push(this);
+      const HomeDataPath = path.join(rootDir, 'data', 'homes.json');
+      fs.writeFile(HomeDataPath, JSON.stringify(RegisteredHomes), err => {
+        console.log("File written successfully");
+      })
+    })
+    // RegisteredHomes.push(this);
+    // const HomeDataPath = path.join(rootDir, 'data', 'homes.json');
+    // fs.writeFile(HomeDataPath, JSON.stringify(RegisteredHomes), err => {
+    //   console.log("File written successfully");
+    // })
+  }
+
+  static fetchAll(callback) {
+    const HomeDataPath = path.join(rootDir, 'data', 'homes.json');
+    const FilePath = fs.readFile(HomeDataPath, (err, data)=> {
+      // console.log("File read : ", err, data);
+      // USing Smart way
+      callback(!err ? JSON.parse(data) : [])
+
+      // if (!err) {
+      //   callback(JSON.parse(data));
+      // } else {
+      //   callback([]);
+      // }
+    // Error because of 2 headers set
+    // callback([]);
+    })
+  }
+}
